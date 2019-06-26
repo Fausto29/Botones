fa793689715fd59df2224de783995cec20f2c900
Fausto29 <danielgilaguilera@hotmail.com>
2019-06-26 11:32:15 +0200

    prueba satisfactoria

--- js/botones_practica.js
+++ js/botones_practica.js
@@ -10,3 +10,28 @@ function ligthsOff (){
 	console.log('he pasado el boton3off');
 } 
 
+function comprobacion (x){
+	if (pulsado!=GameComputer[indice]){
+		error=1;
+		console.log("error en condicional boton"+error);
+	}else {
+		indice++;
+		console.log("sin error"+indice);}
+}
+
+var indice=0;
+var GameComputer=[];
+GameComputer[0]=0;
+
+document.getElementById('boton0').onclick=function(){
+	pulsado=0;
+	var objBoton = document.getElementById('boton0').src="img/boton0.png";
+	console.log('he pasado el encendido del boton0 por presion');
+	setTimeout(function(){
+			ligthsOff();
+		},500);
+	var objAudio = document.getElementById('audioBoton0');
+	objAudio.play();
+	comprobacion (indice);
+	console.log("quiero sabe el indice despues de la funcion "+indice)	
+}
